426c68161f6690386e69cccec65c8ef0048afd9f
praneethkumarpidugu <pidugu2007@gmail.com>
2017-03-20 19:20:45 -0400

    [Our First Unit Test] 1.) Testing Angularjs controller by picking up scope.

--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -1,3 +1,34 @@
 /**
  * Created by praneethkumar on 19/03/17.
  */
+// describe use to declare test suite in jasmine.
+
+describe('Testing Angular JS Test Suite', function () {
+    describe('Testing AngularJS Controller', function () {
+
+        //it block is used to describe single unit test
+        it('should initialize title in scope', function () {
+            //Include angular app into our tests.
+            module('testingAngularApp');
+
+            //Lets initialize a empty scope object.
+            var scope = {};
+
+            // controller variable;
+            var ctrl;
+
+            // Now we will inject angular components into our tests.
+            // In this case we need our controller.
+            // we will $controller service.
+            inject(function ($controller) {
+                //We will controller variable ctrl to our angular controller
+                ctrl = $controller('testingAngularCtrl', {$scope: scope});
+            });
+
+            // We will check if the title is defined our not
+            expect($scope.title).tobeDefined();
+            expect($scope.title).tobe("Testing Angular js Applications");
+            
+        })
+    });
+});
