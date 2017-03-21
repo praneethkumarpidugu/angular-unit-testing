3f52f7d0657375aa1e85d927d01b2bb93ff6ea77
praneethkumarpidugu <pidugu2007@gmail.com>
2017-03-20 23:08:13 -0400

    [Testing Controllers and Scope] Completed

--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -28,6 +28,33 @@ describe('Testing Angular JS Test Suite', function () {
             expect(scope.title).toBeDefined();
             expect(scope.title).toBe("Testing Angular js Applications");
             
-        })
+        });
+
+        it('should add two destinations to destination lists', function () {
+            expect(scope.destinations).toBeDefined();
+            expect(scope.destinations.length).toBe(0);
+
+            scope.newDestination = {
+                city:"London",
+                country:"England"
+            };
+
+            scope.addDestination();
+
+            expect(scope.destinations.length).toBe(1);
+            expect(scope.destinations[0].city).toBe("London");
+            expect(scope.destinations[0].country).toBe("England");
+
+            scope.newDestination.city = "Frankfurt";
+            scope.newDestination.country = "Germany";
+
+            scope.addDestination();
+
+            expect(scope.destinations.length).toBe(2);
+            expect(scope.destinations[1].city).toBe("Frankfurt");
+            expect(scope.destinations[1].country).toBe("Germany");
+            expect(scope.destinations[0].city).toBe("London");
+            expect(scope.destinations[0].country).toBe("England");
+        });
     });
 });
