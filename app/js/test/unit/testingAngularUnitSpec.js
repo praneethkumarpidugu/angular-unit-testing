3f77516b6cf5e1d1162adff9a3d58d07f7a1aabf
praneethkumarpidugu <pidugu2007@gmail.com>
2017-03-20 20:30:51 -0400

    [beforeEach for a more clean code]

--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -4,26 +4,25 @@
 // describe use to declare test suite in jasmine.
 
 describe('Testing Angular JS Test Suite', function () {
+    //Include angular app into our tests.
+    beforeEach(module('testingAngularApp'));
     describe('Testing AngularJS Controller', function () {
+        var scope, ctrl;
+        beforeEach(inject(function ($controller, $rootScope) {
+            scope = $rootScope.$new();
+            //We will controller variable ctrl to our angular controller
+            ctrl = $controller('testingAngularCtrl', {$scope: scope});
+        }));
 
+        afterEach(function () {
+           //cleanup code.
+        });
         //it block is used to describe single unit test
         it('should initialize title in scope', function () {
-            //Include angular app into our tests.
-            module('testingAngularApp');
+
 
             //Lets initialize a empty scope object.
-            var scope = {};
 
-            // controller variable;
-            var ctrl;
-
-            // Now we will inject angular components into our tests.
-            // In this case we need our controller.
-            // we will $controller service.
-            inject(function ($controller) {
-                //We will controller variable ctrl to our angular controller
-                ctrl = $controller('testingAngularCtrl', {$scope: scope});
-            });
 
             // We will check if the title is defined our not
             expect(scope.title).toBeDefined();
